2b67e6b202047aac6bdb01d81520ab66918f5664
Vinicius Colutti <viniciuscolutti@hotmail.com>
2020-12-13 20:28:55 -0300

    adicionando comunicação via Feign

--- src/main/java/com/colutti/hrpayroll/services/PaymentService.java
+++ src/main/java/com/colutti/hrpayroll/services/PaymentService.java
@@ -1,33 +1,20 @@
 package com.colutti.hrpayroll.services;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
 import com.colutti.hrpayroll.entities.Payment;
 import com.colutti.hrpayroll.entities.Worker;
+import com.colutti.hrpayroll.feignClients.WorkerFeignClient;
 
 @Service
 public class PaymentService {
 
-	@Value("${hr-worker.host}")
-	private String workerHost;
-
 	@Autowired
-	private RestTemplate restTemplate;
+	private WorkerFeignClient workerFeignClient;
 
 	public Payment getPayments(Long id, int days) {
-
-		Map<String, String> uriVariables = new HashMap<>();
-
-		uriVariables.put("id", "" + id);
-
-		Worker worker = restTemplate.getForObject(workerHost + "/workers/{id}", Worker.class, uriVariables);
-
+		Worker worker = workerFeignClient.findById(id).getBody();
 		return new Payment(worker.getName(), worker.getDailyIncome(), days);
 	}
 }
